773498e86bc5640411fd9b1ba14a54392588f33f
N G <nicholas.gerstle@cognite.com>
2019-08-09 10:34:22 +0200

    working image pull

--- setup.sh
+++ setup.sh
@@ -12,20 +12,23 @@ export KUBECONFIG="$(k3d get-kubeconfig --name='k3s-default')"
 kubectl get pods --all-namespaces
 
 
+
+
 #insert gcp cred for image pull, per http://docs.heptio.com/content/private-registries/pr-gcr.html
-    # --docker-password="$(gcloud auth print-access-token)" \
-    # --docker-username=oauth2accesstoken \
+    # --docker-username=_json_key \
+    # --docker-password="$(cat k3d-gcr-access.json)" \
 kubectl --namespace=default create secret docker-registry registry-secret \
-    --docker-server=https://gcr.io \
-    --docker-username=_json_key \
-    --docker-password="$(cat k3d-gcr-access.json)"
-    --docker-email=serviceaccount_k3d_gcr_read@cognite.com #TODO
+    --docker-server=https://eu.gcr.io \
+    --docker-password="$(gcloud auth print-access-token)" \
+    --docker-username=oauth2accesstoken \
+    --docker-email=k3d-gcr-read@cognitedata.iam.gserviceaccount.com
 
 #wait for service account to come online
-# sleep 15
-# kubectl get serviceaccounts
-# kubectl patch serviceaccount default  -p '{"imagePullSecrets": [{"name": "registry-secret"}]}'
-# echo "account patched"
+sleep 15
+kubectl get serviceaccounts
+kubectl patch serviceaccount default  -p '{"imagePullSecrets": [{"name": "registry-secret"}]}'
+echo "account patched"
+sleep 10
 
 kubectl apply -f local-service.yaml,local-deployment.yaml
 kubectl apply -f redis-service.yaml,redis-deployment.yaml
@@ -37,6 +40,9 @@ kubectl apply -f redis-service.yaml,redis-deployment.yaml
 #TODO push mockOIDC?
 
 sleep 5 
+kubectl get pods --all-namespaces
+
+sleep 15
 kubectl get pods --all-namespaces
 
 echo "waiting to delete"
